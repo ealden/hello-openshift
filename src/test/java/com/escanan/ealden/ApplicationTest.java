cfea136031f32a08360b8f528cd69a1449ea2527
Ealden Esto E. Escañan <ealden@escanan.com>
2022-06-24 08:26:48 +0800

    Dependency: spring-boot-starter-web

---
+++ src/test/java/com/escanan/ealden/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.escanan.ealden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
