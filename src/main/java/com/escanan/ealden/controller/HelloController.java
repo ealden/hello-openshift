4cd301c6e74862a2ffa75afbba4f91864f939e22
Ealden Esto E. Escañan <ealden@escanan.com>
2022-06-24 08:26:55 +0800

    GET /

---
+++ src/main/java/com/escanan/ealden/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.escanan.ealden.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello, OpenShift!";
+    }
+}
